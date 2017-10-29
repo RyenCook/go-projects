5c65b83d23ef98c9a0e8a09060ade49867934b86
Ryan Cook <ryencook@gmail.com>
2017-10-29 00:57:57 -0400

    Added variables

---
+++ variables/var.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+// Do I hate Go? Found out next week on DBZ!
+func main() {
+    var a, b ,c = 3, 2.0, "Hello"
+    fmt.Println(float64(a) * b);
+    fmt.Println(c + " World");
+}
